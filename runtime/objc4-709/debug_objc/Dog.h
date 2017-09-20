0b2d3b94e4e724209d5c885461d9ab767f72d746
wangweihu <wangweihu@jiayuan.com>
2017-09-20 15:41:24 +0800

    创建一个子类

---
+++ runtime/objc4-709/debug_objc/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  objc
+//
+//  Created by 王伟虎 on 2017/9/20.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ runtime/objc4-709/debug_objc/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  objc
+//
+//  Created by 王伟虎 on 2017/9/20.
+//
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
