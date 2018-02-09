d27dca89017c48218e565f05fa048753101cb766
wangweihu <wangweihu@jiayuan.com>
2018-02-09 18:03:04 +0800

    taggedpointer

--- Runtime源码/objc4-709/debug_objc/main.m
+++ Runtime源码/objc4-709/debug_objc/main.m
@@ -38,9 +38,40 @@ void objc_associatedObjectTest() {
     NSLog(@"name=%@,age=%@,school=%@", name, age, school);
 }
 
+void TaggedPointerTest() {
+    
+    // https://www.cnblogs.com/xiaosong666/p/5045494.html
+    // http://www.360doc.com/content/16/0824/17/12278201_585635115.shtml
+    // http://www.infoq.com/cn/articles/deep-understanding-of-tagged-pointer
+    // https://www.cnblogs.com/bbqzsl/p/5118905.html
+    // http://blog.csdn.net/Xiejunyi12/article/details/61195716
+    // (2^31-1) 0xxxx27
+    // 2^31~2^60-1  0xxxx37
+    NSNumber *number1 = @11;
+    NSNumber *number2 = @22;
+    NSNumber *number3 = @33;
+    NSNumber *numberFFFF = @(0xFFFF);
+    
+    NSNumber *numberLager = @(MAXFLOAT);
+    NSString *string1 = [NSString stringWithUTF8String:"ab"];
+    NSString *string2 = [NSString stringWithUTF8String:"bc"];
+    NSString *string3 = [NSString stringWithUTF8String:"cd"];
+    
+    NSLog(@"string1 pointer is %p", string1);
+    NSLog(@"string2 pointer is %p", string2);
+    NSLog(@"string3 pointer is %p", string3);
+    
+    NSLog(@"number1 pointer is %p", number1);
+    NSLog(@"number2 pointer is %p", number2);
+    NSLog(@"number3 pointer is %p", number3);
+    NSLog(@"numberFFFF pointer is %p", numberFFFF);
+    NSLog(@"numberLager pointer is %p", numberLager);
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
+        TaggedPointerTest();
     }
     return 0;
 }
