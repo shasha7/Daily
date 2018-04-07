4b07704c34dc014968a2b545f931f9996d615431
Mr Wang <792930363@qq.com>
2018-04-07 21:59:12 +0800

    快速排序

--- Algorithm/Algorithm/main.m
+++ Algorithm/Algorithm/main.m
@@ -54,16 +54,32 @@ void inverseStr(char *sourceStr, char *destinationStr) {
     strncat(destinationStr, sourceStr, 1);
 }
 
+//快速排序 https://blog.csdn.net/morewindows/article/details/6684558
+void quick_sort(int *array, int begin, int end) {
+    if (begin < end)
+    {
+        //Swap(s[l], s[(l + r) / 2]); //将中间的这个数和第一个数交换 参见注1
+        int i = begin, j = end, x = array[begin];
+        while (i < j) {
+            while(i < j && array[j] >= x) // 从右向左找第一个小于x的数
+                j--;
+            if(i < j)
+                array[i++] = array[j];
+            
+            while(i < j && array[i] < x) // 从左向右找第一个大于等于x的数
+                i++;
+            if(i < j)
+                array[j--] = array[i];
+        }
+        array[i] = x;
+        quick_sort(array, begin, i - 1); // 递归调用
+        quick_sort(array, i + 1, end);
+    }
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-//        char first = getFirstSingleElement(NULL);
-//        printf("first = %c\n",first);
-        
-//        char *sourceStr = "wangweihu";
-//        char destinationStr[1024] = {0};
-//        inverseStr(sourceStr, destinationStr);
-//        printf("destinationStr = %s \n", destinationStr);
     }
     return 0;
 }
