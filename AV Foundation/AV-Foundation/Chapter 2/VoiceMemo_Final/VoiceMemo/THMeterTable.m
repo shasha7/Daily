9bd3fb697527611cea8c291414873d55281ffabf
wangweihu <wangweihu@jiayuan.com>
2018-04-19 18:20:42 +0800

    优化

--- AV Foundation/AV-Foundation/Chapter 2/VoiceMemo_Final/VoiceMemo/THMeterTable.m
+++ AV Foundation/AV-Foundation/Chapter 2/VoiceMemo_Final/VoiceMemo/THMeterTable.m
@@ -1,31 +1,6 @@
-//
-//  MIT License
-//
-//  Copyright (c) 2014 Bob McCune http://bobmccune.com/
-//  Copyright (c) 2014 TapHarmonic, LLC http://tapharmonic.com/
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
-//
-// This code is based on Apple's MeterTable.cpp code used by the avTouch and SpeakHere
-// sample projects.  It creates an internal table storing the precomputed db -> linear
-// values.
-
+/*
+ AVAudioPlayer、AVAudioRecoder 最强大的功能就是音频进行测量
+ */
 #import "THMeterTable.h"
 
 #define MIN_DB -60.0f
@@ -36,11 +11,11 @@
     NSMutableArray *_meterTable;
 }
 
-- (id)init {
+- (instancetype)init {
     self = [super init];
     if (self) {
         float dbResolution = MIN_DB / (TABLE_SIZE - 1);
-
+        
         _meterTable = [NSMutableArray arrayWithCapacity:TABLE_SIZE];
         _scaleFactor = 1.0f / dbResolution;
 
